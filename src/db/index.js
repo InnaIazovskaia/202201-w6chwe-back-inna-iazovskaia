b6f921a565066b767aa49d13fc5ad4a44278a842
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-19 01:01:52 +0100

    created connectDataBase in src/db/index.js

---
+++ src/db/index.js
@@ -0,0 +1,17 @@
+const chalk = require("chalk");
+const mongoose = require("mongoose");
+const debug = require("debug")("my-robots:database");
+
+const connectDataBase = (connectingString) =>
+  new Promise((resolve, reject) => {
+    mongoose.connect(connectingString, (error) => {
+      if (error) {
+        reject(new Error(`Database not connected: ${error.message}`));
+        return;
+      }
+      debug(chalk.red("Database connected"));
+      resolve();
+    });
+  });
+
+module.exports = connectDataBase;
