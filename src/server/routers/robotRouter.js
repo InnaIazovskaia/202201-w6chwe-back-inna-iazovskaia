5defba872e3e79e643ed1c95b8345c716b25cfe2
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-21 21:40:13 +0100

    created router create in robotRouter.js

--- src/server/routers/robotRouter.js
+++ src/server/routers/robotRouter.js
@@ -1,8 +1,20 @@
 const express = require("express");
-const getRobots = require("../contrillers/robotsControllers");
+const {
+  getRobots,
+  getRobot,
+  createRobot,
+} = require("../contrillers/robotsControllers");
+const { loginUser } = require("../contrillers/usersControllers");
+const { authUser } = require("../middlewares/authUser");
 
 const router = express.Router();
 
 router.get("/", getRobots);
+
+router.get("/:idRobot", getRobot);
+
+router.post("/create", authUser, createRobot);
+
+router.post("/login", loginUser);
 
 module.exports = router;
