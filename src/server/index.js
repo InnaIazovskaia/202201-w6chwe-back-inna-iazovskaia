71ed74268c6b431bb7b6ecad473475e2b756753f
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-22 17:31:23 +0100

    remooved onServer from server/index.js

--- src/server/index.js
+++ src/server/index.js
@@ -1,6 +1,4 @@
 const express = require("express");
-const debug = require("debug")("my-robots:server");
-const chalk = require("chalk");
 const morgan = require("morgan");
 const helmet = require("helmet");
 const cors = require("cors");
@@ -9,27 +7,14 @@ const robotsRouter = require("./routers/robotRouter");
 
 const app = express();
 
-const onServer = (port) =>
-  new Promise((resolve, reject) => {
-    const server = app.listen(port, () => {
-      debug(chalk.green(`Server can hear you in http://localhost${port}`));
-      resolve();
-    });
-
-    server.on("error", (error) => {
-      reject(new Error(`Error on server: ${error.message}`));
-    });
-  });
-
-app.use(morgan("dev"));
-app.use(express.json());
-app.use(helmet());
-
 app.use(cors());
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(express.json());
 
 app.use("/robots", robotsRouter);
 
 app.use(notFoundError);
 app.use(globalError);
 
-module.exports = onServer;
+module.exports = app;
