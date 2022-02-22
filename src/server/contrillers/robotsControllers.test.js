1714dbdf2076f4d24a9c8868b8957835e6608c6c
Inna Iazovkaia <inna.0961@mail.ru>
2022-02-22 19:10:26 +0100

    createt test for getRobot controler in robotsControler.js

--- src/server/contrillers/robotsControllers.test.js
+++ src/server/contrillers/robotsControllers.test.js
@@ -1,7 +1,10 @@
 const Robot = require("../../db/models/Robot");
-const getRobots = require("./robotsControllers");
+const { getRobots, getRobot } = require("./robotsControllers");
 
 describe("Given a getRobots controller", () => {
+  beforeEach(() => {
+    jest.resetAllMocks();
+  });
   describe("When it receves a response", () => {
     test("Then it should call method json with array of robots of the received response", async () => {
       const res = {
@@ -12,6 +15,10 @@ describe("Given a getRobots controller", () => {
           id: "2022",
           name: "Bender",
         },
+        {
+          id: "2022",
+          name: "Fender",
+        },
       ];
       Robot.find = jest.fn().mockResolvedValue(robots);
 
@@ -19,6 +26,53 @@ describe("Given a getRobots controller", () => {
 
       expect(Robot.find).toHaveBeenCalled();
       expect(res.json).toHaveBeenCalledWith({ robots });
+    });
+  });
+});
+
+describe("Given a getRobot controller", () => {
+  describe("When it receives a response with valid id", () => {
+    test("Then if robot exists it should call method json withe the robot", async () => {
+      const req = {
+        params: {
+          id: "superrobot",
+        },
+      };
+      const res = {
+        json: jest.fn(),
+      };
+      const next = jest.fn();
+      const robot = {
+        id: "superrobot",
+        name: "Uran",
+        image: "",
+        speed: 5,
+        strenght: 3,
+        date_of_creation: "01-01-2022",
+      };
+
+      Robot.findById = jest.fn().mockResolvedValue(robot);
+
+      await getRobot(req, res, next);
+
+      expect(res.json).toHaveBeenCalledWith({ robot });
+      expect(next).not.toHaveBeenCalled();
+    });
+
+    test("Then if the id format is invalid it should call next", async () => {
+      const req = {
+        params: {
+          id: "superrobot",
+        },
+      };
+      const next = jest.fn();
+      const error = new Error("Incorrect id");
+
+      Robot.findById = jest.fn().mockRejectedValue(error);
+
+      await getRobot(req, null, next);
+
+      expect(next).toHaveBeenCalledWith(error);
     });
   });
 });
